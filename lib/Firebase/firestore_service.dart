3d933486b0377417ff886ae747df35bb2ce854a0
Martin <schafi350@gmail.com>
2024-04-27 13:39:18 +0200

    Jo Jo Jo
    was macht flo nochmal?

--- lib/Firebase/firestore_service.dart
+++ lib/Firebase/firestore_service.dart
@@ -3,8 +3,39 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<String> retrieveText() async {
-    DocumentSnapshot docSnapshot = await _firestore.doc('Arbeitsblätter/WXoxYKcgK9xHd5B0mO5V').get();
+  Future<String> retrieveText(String docId) async {
+    DocumentSnapshot docSnapshot =
+    await _firestore.doc('Arbeitsblätter/$docId').get();
     return docSnapshot.get('Text') as String;
+  }
+
+  Future<void> addMissingWord(String docId, String word, int position) async {
+    await _firestore.doc('Arbeitsblätter/$docId/Lücken/Lücke$position').set({
+      'word': word,
+      'position': position,
+    });
+  }
+
+  Future<List<Map<String, dynamic>>> getMissingWords(String docId) async {
+    QuerySnapshot querySnapshot = await _firestore.collection('Arbeitsblätter/$docId/Lücken').get();
+    return querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+  }
+
+
+  Future<String> createTextWithGaps(String docId) async {
+    String text = await retrieveText(docId);
+    List<Map<String, dynamic>> missingWords = await getMissingWords(docId);
+    List<String> words = text.split(' ');
+
+    for (var missingWord in missingWords) {
+      if (missingWord['Position'] != null) { // Change 'position' to 'Position'
+        int position = missingWord['Position']; // Change 'position' to 'Position'
+        if (position < words.length) {
+          words[position] = '___';
+        }
+      }
+    }
+
+    return words.join(' ');
   }
 }
--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -6,7 +6,7 @@ import 'package:physicstool/screens/grid_screen.dart';
 import 'package:physicstool/screens/text_screen.dart';
 
 class UserPage extends StatelessWidget {
-  final Future<String> text = FirestoreService().retrieveText();
+  final Future<String> text = FirestoreService().createTextWithGaps('WXoxYKcgK9xHd5B0mO5V');
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,7 @@ class UserPage extends StatelessWidget {
       future: text,
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
+          return Container();
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else {
@@ -26,27 +26,24 @@ class UserPage extends StatelessWidget {
 }
 
 class FullHeightTextPage extends StatelessWidget {
-  final String text; // This will be the text retrieved from Firebase Firestore
+  final String text;
 
   FullHeightTextPage({required this.text});
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double horizontalPadding = screenWidth / 3; // Calculate the horizontal padding
-    double verticalPadding = 20.0; // Set the vertical padding
-    double textSize = 24.0; // Set the text size
+    double horizontalPadding = screenWidth / 3;
+    double verticalPadding = 20.0;
+    double textSize = 24.0;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding), // Add padding here
-            child: Text(
-              text,
-              style: TextStyle(fontSize: textSize), // Increase the text size here
-            ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
+        child: SingleChildScrollView(
+          child: Text(
+            text,
+            style: TextStyle(fontSize: textSize),
           ),
         ),
       ),
